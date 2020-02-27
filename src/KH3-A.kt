e16e85b870cd78f930385f7f16d1d4d8059692d0
Edi Carreras <edi@poquesoft.net>
2020-02-27 15:02:17 +0100

    A

--- src/KH3-A.kt
+++ src/KH3-A.kt
@@ -1,6 +1,24 @@
+import java.lang.Math.round
+import kotlin.math.roundToLong
 
 fun main() {
     val n = readLine()!!.toInt()
-    val (a, b) = readLine()!!.split(' ').map(String::toInt)
-    val list = readLine()!!.split(' ').map(String::toInt)
+    repeat(n){
+        val u = readLine()!!.toLong()
+        format(u)
+    }
+}
+
+fun format(u: Long) {
+    when (u){
+        in (0..999) -> println(u)
+        in (1000..999499) -> {
+            val k = (u+500) / 1000
+            println ("${k}K")
+        }
+        else -> {
+            val m = (u+500000) / 1000000
+            println ("${m}M")
+        }
+    }
 }
